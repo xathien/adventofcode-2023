2623b70f133e7a2ed89dc037617ba4fe93916263
Zack Young <zack.young@spiff.com>
2023-12-22 00:16:42 -0700

    Tidy up Day 22, add some EoF newlines

--- day22/pt1.rb
+++ day22/pt1.rb
@@ -1,13 +1,9 @@
+# blocks = File.readlines('test_input')
 blocks = File.readlines('input')
     .map.with_index { |line, index|
       x0, y0, z0, x1, y1, z1 = line.match(/(\d+),(\d+),(\d+)~(\d+),(\d+),(\d+)/).captures.map(&:to_i)
-      axis = case
-      when x0 != x1 then 0
-      when y0 != y1 then 1
-      when z0 != z1 then 2
-      end
       block_num = index + 1
-      block = [x0..x1, y0..y1, z0..z1, block_num]
+      [x0..x1, y0..y1, z0..z1, block_num]
     }.sort_by { |block| block[2].begin }
 
 floor_levels = Hash.new { |h, k| h[k] = [0, nil] }
@@ -17,8 +13,7 @@ supported_by = Hash.new { |h, k| h[k] = Set.new }
 blocks.each { |block|
   xr, yr, zr, block_num = block
   h = zr.end - zr.begin + 1
-  highest_floor = 0
-  floors = xr.flat_map{ |x|
+  floors = xr.flat_map { |x|
     yr.map { |y|
       floor_levels[[x, y]] + [x, y]
     }
